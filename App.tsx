898f5fafca79517b8823fdf78f516feefab64e89
vakas-786 <vakas0959@gmail.com>
2021-01-02 18:26:18 -0500

    amplify auth

--- App.tsx
+++ App.tsx
@@ -7,9 +7,10 @@ import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 import Amplify from 'aws-amplify'
 import config from './aws-exports'
+import { withAuthenticator } from 'aws-amplify-react-native'
 Amplify.configure(config)
 
-export default function App() {
+function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
 
@@ -24,3 +25,6 @@ export default function App() {
     );
   }
 }
+
+export default withAuthenticator(App)
+
