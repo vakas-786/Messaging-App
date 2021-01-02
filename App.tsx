7c04bbcb5f81d71108da22c3f27fc44df0d7aaa7
vakas-786 <vakas0959@gmail.com>
2021-01-02 18:50:08 -0500

    untrack gitignore files

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
-import Amplify from 'aws-amplify'
+import Amplify, {Auth} from 'aws-amplify'
 import config from './aws-exports'
 import { withAuthenticator } from 'aws-amplify-react-native'
 Amplify.configure(config)
